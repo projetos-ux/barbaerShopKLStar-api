95a9e99ab342c0796ab161a5cabe5c107e7f8c64
Samuel Henrique <henriquesamuel965@gmail.com>
2021-02-11 13:37:36 -0300

    create pekage with Spring

---
+++ BarberShopKLStar/src/main/java/com/barberShopKLStar/BarberShopKLStar/BarberShopKlStarApplication.java
@@ -0,0 +1,13 @@
+package com.barberShopKLStar.BarberShopKLStar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberShopKlStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberShopKlStarApplication.class, args);
+	}
+
+}
---
+++ BarberShopKLStar/src/test/java/com/barberShopKLStar/BarberShopKLStar/BarberShopKlStarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barberShopKLStar.BarberShopKLStar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberShopKlStarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
