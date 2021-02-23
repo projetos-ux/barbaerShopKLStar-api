8129b72a1b8766eb93d6033f23311ba5e1a770ce
Samuel Henrique <samuel.henrique@dcx.com.br>
2021-02-22 22:50:31 -0300

    refs (#1) - Conclusão da classe registrar.

--- BarberShopKLStar/src/main/java/com/barberShopKLStar/BarberShopKLStar/user/controller/UserController.java
+++ BarberShopKLStar/src/main/java/com/barberShopKLStar/BarberShopKLStar/user/controller/UserController.java
@@ -1,8 +1,33 @@
 package com.barberShopKLStar.BarberShopKLStar.user.controller;
 
-import org.springframework.stereotype.Controller;
+import javax.validation.Valid;
 
-@Controller
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.barberShopKLStar.BarberShopKLStar.user.dto.OutRegisterUserDTO;
+import com.barberShopKLStar.BarberShopKLStar.user.dto.RegisterUserDTO;
+import com.barberShopKLStar.BarberShopKLStar.user.service.RegisterUserServiceImpl;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/klstar/")
+@CrossOrigin("*")
 public class UserController {
-
+	
+	private final RegisterUserServiceImpl registerImpl;	
+	
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping(value = "/register")
+	public OutRegisterUserDTO registerUser(@Valid @RequestBody RegisterUserDTO user) {
+		return registerImpl.save(user);
+		
+	}
 }
--- BarberShopKLStar/src/main/java/com/barberShopKLStar/BarberShopKLStar/user/service/RegisterUserServiceImpl.java
+++ BarberShopKLStar/src/main/java/com/barberShopKLStar/BarberShopKLStar/user/service/RegisterUserServiceImpl.java
@@ -1,1 +1,51 @@
+package com.barberShopKLStar.BarberShopKLStar.user.service;
 
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.barberShopKLStar.BarberShopKLStar.user.dto.OutRegisterUserDTO;
+import com.barberShopKLStar.BarberShopKLStar.user.dto.RegisterUserDTO;
+import com.barberShopKLStar.BarberShopKLStar.user.entity.User;
+import com.barberShopKLStar.BarberShopKLStar.user.exception.InvalidDataException;
+import com.barberShopKLStar.BarberShopKLStar.user.exception.UserAlreadyExistsException;
+import com.barberShopKLStar.BarberShopKLStar.user.jpa.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+@Service
+public class RegisterUserServiceImpl implements RegisterUserService {
+	
+	private final UserRepository userBD;
+	
+
+
+	
+	@Transactional
+	public OutRegisterUserDTO save(RegisterUserDTO user) {	
+		
+		if(userBD.existsByLogin(user.getLogin())==true){
+			throw new UserAlreadyExistsException();	
+		}
+		if(userBD.existsByEmail(user.getEmail())==true) {
+			throw new UserAlreadyExistsException();
+		}
+		if(user.getLogin().matches("[a-zA-Z.]*")) {
+			String EncryptedPassword = user.getPassword();
+			user.setPassword(EncryptedPassword);
+			User userImpl = User.to(user);
+			userBD.save(userImpl);
+		
+		
+		return new OutRegisterUserDTO(userImpl);
+		}
+		
+		else {
+			throw new InvalidDataException();
+		}
+	}
+	
+}
